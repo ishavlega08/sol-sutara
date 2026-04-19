213d0fdc30fbea0617cad8fdbfe33b8d27b419ef
Ishav <ishavlega@gmail.com>
2026-04-19 17:53:50 +0530

    feat: add CORS support and restructure API routes

--- backend/api/src/index.ts
+++ backend/api/src/index.ts
@@ -1,12 +1,15 @@
 import "dotenv/config";
 import express from "express";
-import componentRoutes from "./routes/component.routes";
+import cors from "cors";
+import routes from "./routes";
+
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
-app.use("/components", componentRoutes);
+app.use("/api", routes);
 
 app.listen(PORT, () => {
     console.log(`Sol Sutara API running on port ${PORT}`);
